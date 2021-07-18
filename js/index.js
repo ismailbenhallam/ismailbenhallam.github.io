0202262ebc3ac9df22356724bb534f00ecb09052
Ismaïl BENHALLAM <ibenhallam@smb.sqli.com>
2021-07-18 15:16:32 +0100

    Start using materialize

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 for (let item of document.getElementsByClassName("myName")) {
-  console.log((item.textContent = myName));
+  item.textContent = myName;
 }
+
+// Floating Action Button
+let toTop = document.getElementById("toTop");
+toTop.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+});
+const HEADER_HEIGHT = 500;
+window.addEventListener("scroll", function () {
+  if (
+    document.body.scrollTop > HEADER_HEIGHT ||
+    document.documentElement.scrollTop > HEADER_HEIGHT
+  ) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+});
--- js/materialize-Initializations.js
+++ js/materialize-Initializations.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, options);
+  var instances = M.Sidenav.init(elems);
 });
 
-// Or with jQuery
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".materialboxed");
+  var instances = M.Materialbox.init(elems);
+});
 
-$(document).ready(function () {
-  $(".sidenav").sidenav();
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".parallax");
+  var instances = M.Parallax.init(elems);
 });
