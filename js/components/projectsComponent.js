3df620deaf1d5cd785274c110b48c4f5b381e7a9
Ismaïl BENHALLAM <ismailben44@gmail.com>
2021-07-31 15:02:39 +0100

    Change colors

--- js/components/projectsComponent.js
+++ js/components/projectsComponent.js
@@ -3,7 +3,7 @@ projects.forEach((p) => {
   projectsWrapper.insertAdjacentHTML(
     "beforeend",
     `<div class="col s12 m6">
-        <div class="card large project-card hoverable">
+        <div class="card medium project-card hoverable">
             <div class="project-image-wrapper card-image ${
               p.details ? "waves-effect waves-block waves-light" : ""
             }">
